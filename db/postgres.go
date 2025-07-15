aeab1bef97f746dd806f01b8777fa10232fcf45e
nahima97 <nahima.sultana@gmail.com>
2025-07-15 20:27:50 +0100

    database connection and migration

--- db/postgres.go
+++ db/postgres.go
@@ -1,17 +1,37 @@
 package db
 
+import (
+	"fmt"
+	"library/models"
+	"log"
+	"os"
 
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
-//alina
+var Db *gorm.DB
 
-func InitDb () {
+func InitDb() {
 
+	var err error
 
-	//connect to database 
+	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
 
+	Db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("connected to database successfully!")
 
-
-	//migrate schema 
-
-	
+	err = Db.AutoMigrate(&models.User{}, &models.Book{})
+	if err != nil {
+		log.Fatal("unable to migrate schema", err)
+	}
 }
