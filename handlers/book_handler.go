d27b107dd7627e9183cc98cd2d7750eb0b04ff3f
Safa <safabintabdikarin@gmail.com>
2025-07-15 19:49:51 +0100

    added AddBook endpoint

--- handlers/book_handler.go
+++ handlers/book_handler.go
@@ -1,39 +1,55 @@
 package handlers
 
-import "library/services"
+import (
+	"encoding/json"
+	"library/models"
+	"library/services"
+	"net/http"
+)
 
 type BookHandler struct {
 	Service *services.BookService
 }
 
-
-func () SearchBook() {
-//all - guests, users
-
+func  SearchBook() {
+	//all - guests, users
 
 }
 
-func () BorrowBook() {
-//users
-//check if user is old enough
-//delete from the users slice of books 
+func BorrowBook() {
+	//users
+	//check if user is old enough
+	//delete from the users slice of books
 
 }
 
-func () ReturnBook() {
-//users
-//append the users slice of books 
+func ReturnBook() {
+	//users
+	//append the users slice of books
 
 }
 
-func () AddBook() {
-//only admin 
+func AddBook(w http.ResponseWriter, r *http.Request) {
+	// only admin
+	userClaims := r.Context().Value(middleware.UserKey).(*Claims)
 
+	if !userClaims.IsAdmin {
+		http.Error(w, "Access denied: admin only", http.StatusForbidden)
+		return
+	}
+
+	var book models.Book
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	database.DB.Add(&book)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(book)
 
 }
 
-func () DeleteBook() {
-//only admin
-
+func DeleteBook() {
+	//only admin
 
 }
